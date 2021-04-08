3448fdb978dc62868a193b7181f0fb63b7ea7bc7
Kripa Rajshekhar <70665360+metonymize-kripa@users.noreply.github.com>
2021-04-08 12:41:33 -0500

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -1,6 +1,6 @@
 export default {
-  repository: 'https://github.com/shuding/nextra',
-  titleSuffix: ' – Nextra',
+  repository: 'https://github.com/metonymize-kripa/innovation-analytics-docs',
+  titleSuffix: ' – InnovationAnalytics',
   logo: (
     <>
       <span className="mr-2 font-extrabold hidden md:inline">Innovation Analytics</span>
